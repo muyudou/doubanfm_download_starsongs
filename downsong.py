a0a47a8677ba48bdef7034a31a8d22260a81e963
= <=>
2014-05-27 09:55:27 +0800

    downsong

--- downsong.py
+++ downsong.py
@@ -38,17 +38,16 @@ def down_song(sid, ssid):
     song = playlist['song'][0]
     song_singer = valid_name(song['artist'])
     songname = "%s(%s)" % (valid_name(song['title']), song_singer)  
-    songpath = download_path+'/'+songname+'.mp3'
+    songpath = download_path+songname+'.mp3'
     if sid == song['sid']:
         if os.path.exists(songpath):
 	        print("歌曲已经存在")
         else:
-            print(("下载歌曲：%s\t歌手是:%s\tloading...")%(songname,
-			song_singer)) 
+            print(("下载歌曲：%s\t歌手是:%s\tloading...")%(valid_name(song['title']), song_singer)) 
             try:
                 request.urlretrieve(song['url'], songpath)
             except error.URLError as e:
-                print("下载%s-%s 歌曲发生异常，下载失败！" % (songname, song_singer), e)
+                print("下载%s歌曲发生异常，下载失败！" % songname, e)
             else:
                 print("'%s'下载完成" % songname)
     else:
